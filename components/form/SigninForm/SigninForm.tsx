19113be64ab503592a93ce16338a1c157be0e4a8
kunaalgaur <kunaalgaurr@gmail.com>
2023-09-21 14:06:37 +0530

    Removed formik integration

--- components/form/SigninForm/SigninForm.tsx
+++ components/form/SigninForm/SigninForm.tsx
@@ -1,71 +1,59 @@
 'use client';
 
-import { signinValidations } from '@/lib/validations/signinValidations';
 import styles from './SigninForm.module.css';
-import { useFormik } from 'formik';
 import { useState } from 'react';
 import { useAppDispatch } from '@/redux/hooks';
 import { failure, request, success } from '@/redux/signinSlice';
 import { useRouter } from 'next/navigation';
 import ReactLoading from 'react-loading';
+import toast, { Toaster } from 'react-hot-toast';
 
 const SigninForm = () => {
-  const dispatch = useAppDispatch();
-  const [data, setData] = useState(null);
   const router = useRouter();
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      password: '',
-    },
-    validationSchema: signinValidations,
-    onSubmit: async () => {
-      dispatch(request());
-      try {
-        const response = await fetch('/api/auth/sign-in', {
-          method: 'POST',
-          body: JSON.stringify({
-            email: formik.values.email,
-            password: formik.values.password,
-          }),
-        });
-
-        if (response.ok) {
-          const responseData = await response.json();
-
-          if (responseData.data) {
-            dispatch(
-              success({
-                userId: responseData.data.userId,
-                token: responseData.data.token,
-              })
-            );
-
-            // Saving data in browser local storage
-            localStorage.setItem('userId', responseData.data.userId);
-            localStorage.setItem('token', responseData.data.token);
-          }
-
-          // Check is the user is onboarded or not
-          // If user is onboarder then navigate to '/'
-          if (responseData.data.username) {
-            return router.push('/');
-          }
-
-          // Navigate to '/onboarding' if not onboarded
-          return router.push('/onboarding');
-        }
-      } catch (error: any) {
-        console.error(error);
-        failure(error);
-      }
-    },
-  });
+  const dispatch = useAppDispatch();
 
   const [toggle, setToggle] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    dispatch(request());
+    try {
+      setLoading(true);
+      const credentials = { email: email, password: password };
+      const res = await fetch('/api/auth/sign-in', {
+        method: 'POST',
+        body: JSON.stringify(credentials),
+      });
+      const response = await res.json();
+      if (!res.ok) {
+        return toast.error(response.message);
+      }
+      const user = response.user;
+      const token = response.token;
+      console.log(user);
+      if (res.ok) {
+        dispatch(success({ userId: user._id, token: token }));
+        toast.success(response.message);
+        if (!user.username) {
+          return router.push('/onboarding');
+        } else {
+          return router.push('/');
+        }
+      }
+    } catch (error: any) {
+      dispatch(failure(error));
+      return toast.error(error.message);
+    } finally {
+      return setLoading(false);
+    }
+  };
+
   return (
-    <form action="" id={styles.container} onSubmit={formik.handleSubmit}>
+    <form action="" id={styles.container} onSubmit={handleSubmit}>
+      <Toaster position="top-center" reverseOrder={true} />
       <label htmlFor="email" className={styles.label}>
         <span className={styles.text}>Please enter your email</span>
         <input
@@ -73,25 +61,8 @@ const SigninForm = () => {
           className={styles.input}
           name="email"
           id="email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          style={{
-            border:
-              formik.touched.email && Boolean(formik.errors.email)
-                ? '1px solid tomato'
-                : '1px solid #8080804a',
-            color:
-              formik.touched.email && Boolean(formik.errors.email)
-                ? 'tomato'
-                : 'white',
-          }}
+          onChange={(e) => setEmail(e.target.value)}
         />
-        <span className={styles.helperText}>
-          {formik.touched.email && Boolean(formik.errors.email)
-            ? formik.errors.email
-            : null}
-        </span>
       </label>
       <label htmlFor="password" className={styles.label}>
         <span className={styles.text}>Please create a Password</span>
@@ -100,19 +71,7 @@ const SigninForm = () => {
           className={styles.input}
           name="password"
           id="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          style={{
-            border:
-              formik.touched.password && Boolean(formik.errors.password)
-                ? '1px solid tomato'
-                : '1px solid #8080804a',
-            color:
-              formik.touched.password && Boolean(formik.errors.password)
-                ? 'tomato'
-                : 'white',
-          }}
+          onChange={(e) => setPassword(e.target.value)}
         />
         <label htmlFor="checkbox" id={styles.childLabel}>
           <input
@@ -123,14 +82,9 @@ const SigninForm = () => {
           />
           <span className={styles.text}>Make password visible.</span>
         </label>
-        <span className={styles.helperText}>
-          {formik.touched.password && Boolean(formik.errors.password)
-            ? formik.errors.password
-            : null}
-        </span>
       </label>
-      <button type="submit" id={styles.button} disabled={formik.isSubmitting}>
-        {formik.isSubmitting ? (
+      <button type="submit" id={styles.button} disabled={loading}>
+        {loading ? (
           <ReactLoading type="spin" color="black" height={20} width={20} />
         ) : (
           'Sign in'
