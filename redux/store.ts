cd5c801048d75d749f9d5c728cf1b4b7759a08be
kunaalgaur <kunaalgaurr@gmail.com>
2023-09-22 19:40:16 +0530

    Updated store

--- redux/store.ts
+++ redux/store.ts
@@ -1,10 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
-import signinReducer from './signinSlice';
+import signinReducer from './slice/signinSlice';
+import threadReducer from './slice/threadSlice';
+import signupReducer from './slice/signupSlice';
+import onboardingSlice from './slice/onboardingSlice';
 
 export const store = configureStore({
-  reducer: {
-    signin: signinReducer,
-  },
+    reducer: {
+        signup: signupReducer,
+        signin: signinReducer,
+        thread: threadReducer,
+        onboarding: onboardingSlice,
+    },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
