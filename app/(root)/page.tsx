85aaff5408dd503fb91f8f7bea3abc7a035fb338
kunaalgaur <kunaalgaurr@gmail.com>
2023-09-22 08:45:20 +0530

    Minor changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,22 +2,14 @@ import Thread from '@/components/card/Thread/Thread';
 import styles from './page.module.css';
 import { useAppSelector } from '@/redux/hooks';
 
+const getAllPosts = async () => {
+  const res = await fetch('')
+}
+
 export default function Home() {
   return (
     <div id={styles.container}>
       <div></div>
-      <Thread />
-      <Thread />
-      <Thread />
-      <Thread />
-      <Thread />
-      <Thread />
-      <Thread />
-      <Thread />
-      <Thread />
-      <Thread />
-      <Thread />
-      <Thread />
     </div>
   );
 }
