4cf800fc1ceffa0d283821fbe43846b675d54b35
kunaalgaur <kunaalgaurr@gmail.com>
2023-09-22 19:43:22 +0530

    Final Commit

--- components/form/SigninForm/SigninForm.tsx
+++ components/form/SigninForm/SigninForm.tsx
@@ -2,122 +2,145 @@
 
 import styles from './SigninForm.module.css';
 import { useState } from 'react';
-import { useAppDispatch } from '@/redux/hooks';
-import { failure, request, success } from '@/redux/signinSlice';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import { failure, request, success } from '@/redux/slice/signinSlice';
 import { useRouter } from 'next/navigation';
 import ReactLoading from 'react-loading';
 import toast, { Toaster } from 'react-hot-toast';
 
 const SigninForm = () => {
-  const router = useRouter();
-  const dispatch = useAppDispatch();
+    const router = useRouter();
+    const dispatch = useAppDispatch();
 
-  const [toggle, setToggle] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [email, setEmail] = useState<string>();
-  const [password, setPassword] = useState<string>();
+    const [toggle, setToggle] = useState<boolean>(false);
+    const [email, setEmail] = useState<string>();
+    const [password, setPassword] = useState<string>();
 
-  const handleSubmit = async (e: any) => {
-    // to prevent to page from reloading
-    e.preventDefault();
+    const { loading } = useAppSelector((state) => state.signin);
 
-    // initialize the request reducer
-    dispatch(request());
+    const handleSubmit = async (e: any) => {
+        // to prevent to page from reloading
+        e.preventDefault();
 
-    try {
-      // setting the loading state to true
-      setLoading(true);
+        // initialize the request reducer
+        dispatch(request());
 
-      const credentials = { email: email, password: password };
+        try {
+            const credentials = { email: email, password: password };
 
-      // hitting the signin api
-      const res = await fetch('/api/auth/sign-in', {
-        method: 'POST',
-        body: JSON.stringify(credentials),
-      });
+            // hitting the signin api
+            const res = await fetch('/api/auth/sign-in', {
+                method: 'POST',
+                body: JSON.stringify(credentials),
+            });
 
-      const response = await res.json();
+            const response = await res.json();
 
-      // sending error to front end if the response is incorrect
-      if (!res.ok) {
-        return toast.error(response.message);
-      }
+            // sending error to front end if the response is incorrect
+            if (!res.ok) {
+                toast.error(response.message, {
+                    style: {
+                        borderRadius: '10px',
+                        background: '#333',
+                        color: '#fff',
+                    },
+                    duration: 6000,
+                });
 
-      const user = response.user;
-      const token = response.token;
+                return dispatch(failure(response.message));
+            }
 
-      // saving the response in local storage
-      localStorage.setItem('userId', user._id);
-      localStorage.setItem('token', token);
+            const user = response.user;
+            const token = response.token;
 
-      // implement this logic if the response is okay
-      if (res.ok) {
-        // initializing the success reducer if the api is fetched successfully
-        dispatch(success({ userId: user._id, token: token }));
+            // saving the response in local storage
+            localStorage.setItem('userId', user._id);
+            localStorage.setItem('token', token);
 
-        // send a success message
-        toast.success(response.message);
+            // implement this logic if the response is okay
+            if (res.ok) {
+                // initializing the success reducer if the api is fetched successfully
+                dispatch(success({ userId: user._id, token: token }));
 
-        // redirect the user to '/onboarding' if the user is not onboarded and to '/' is the user is onboarded
-        if (!user.username) {
-          return router.push('/onboarding');
-        } else {
-          return router.push('/');
+                // send a success message
+                toast.success(response.message, {
+                    style: {
+                        borderRadius: '10px',
+                        background: '#333',
+                        color: '#fff',
+                    },
+                    duration: 6000,
+                });
+
+                // redirect the user to '/onboarding' if the user is not onboarded and to '/' is the user is onboarded
+                if (!user.username) {
+                    return router.push('/onboarding');
+                } else {
+                    return router.push('/');
+                }
+            }
+        } catch (error: any) {
+            // initialize the failure reducer
+            dispatch(failure(error));
+
+            // return an error message in front end
+            return toast.error(error.message, {
+                style: {
+                    borderRadius: '10px',
+                    background: '#333',
+                    color: '#fff',
+                },
+                duration: 6000,
+            });
         }
-      }
-    } catch (error: any) {
-      // initialize the failure reducer
-      dispatch(failure(error));
+    };
 
-      // return an error message in front end
-      return toast.error(error.message);
-    } finally {
-      // and finally set loading to false
-      return setLoading(false);
-    }
-  };
-
-  return (
-    <form action="" id={styles.container} onSubmit={handleSubmit}>
-      <Toaster position="top-center" reverseOrder={true} />
-      <label htmlFor="email" className={styles.label}>
-        <span className={styles.text}>Please enter your email</span>
-        <input
-          type="text"
-          className={styles.input}
-          name="email"
-          id="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <label htmlFor="password" className={styles.label}>
-        <span className={styles.text}>Please create a Password</span>
-        <input
-          type={toggle ? 'text' : 'password'}
-          className={styles.input}
-          name="password"
-          id="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <label htmlFor="checkbox" id={styles.childLabel}>
-          <input
-            type="checkbox"
-            name="checkbox"
-            id="checkbox"
-            onChange={() => setToggle(!toggle)}
-          />
-          <span className={styles.text}>Make password visible.</span>
-        </label>
-      </label>
-      <button type="submit" id={styles.button} disabled={loading}>
-        {loading ? (
-          <ReactLoading type="spin" color="black" height={20} width={20} />
-        ) : (
-          'Sign in'
-        )}
-      </button>
-    </form>
-  );
+    return (
+        <form action="" id={styles.container} onSubmit={handleSubmit}>
+            <Toaster position="top-center" reverseOrder={true} />
+            <label htmlFor="email" className={styles.label}>
+                <span className={styles.text}>Please enter your email</span>
+                <input
+                    type="text"
+                    className={styles.input}
+                    name="email"
+                    id="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </label>
+            <label htmlFor="password" className={styles.label}>
+                <span className={styles.text}>Please create a Password</span>
+                <input
+                    type={toggle ? 'text' : 'password'}
+                    className={styles.input}
+                    name="password"
+                    id="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <label htmlFor="checkbox" id={styles.childLabel}>
+                    <input
+                        type="checkbox"
+                        name="checkbox"
+                        id="checkbox"
+                        onChange={() => setToggle(!toggle)}
+                    />
+                    <span className={styles.text}>Make password visible.</span>
+                </label>
+            </label>
+            <button type="submit" id={styles.button} disabled={loading}>
+                {loading ? (
+                    <ReactLoading
+                        type="spin"
+                        color="black"
+                        height={20}
+                        width={20}
+                    />
+                ) : (
+                    'Sign in'
+                )}
+            </button>
+        </form>
+    );
 };
 
 export default SigninForm;
--- components/form/SignupForm/SignupForm.tsx
+++ components/form/SignupForm/SignupForm.tsx
@@ -1,116 +1,148 @@
 'use client';
 
+import { failure, request, success } from '@/redux/slice/signupSlice';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import toast, { Toaster } from 'react-hot-toast';
 import styles from './SignupForm.module.css';
-import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import ReactLoading from 'react-loading';
-import toast, { Toaster } from 'react-hot-toast';
+import { useState } from 'react';
 
 const SignupForm = () => {
-  const [toggle, setToggle] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [name, setName] = useState<string>();
-  const [email, setEmail] = useState<string>();
-  const [password, setPassword] = useState<string>();
-  const router = useRouter();
+    const dispatch = useAppDispatch();
+    const router = useRouter();
 
-  const handleSignup = async (e: any) => {
-    e.preventDefault();
+    const [toggle, setToggle] = useState<boolean>(false);
+    const [password, setPassword] = useState<string>();
+    const [email, setEmail] = useState<string>();
+    const [name, setName] = useState<string>();
 
-    try {
-      setLoading(true);
+    const { loading } = useAppSelector((state) => state.signup);
 
-      const credentials = {
-        name: name,
-        email: email,
-        password: password,
-      };
+    const handleSignup = async (e: any) => {
+        // to stop the page from reloading in submitting
+        e.preventDefault();
 
-      const res = await fetch('/api/auth/sign-up', {
-        method: 'POST',
-        body: JSON.stringify(credentials),
-      });
+        // dispatch the signin request
+        dispatch(request());
 
-      const response = await res.json();
+        try {
+            const credentials = {
+                name: name,
+                email: email,
+                password: password,
+            };
 
-      console.log(response);
+            // making a post request
+            const res = await fetch('/api/auth/sign-up', {
+                method: 'POST',
+                body: JSON.stringify(credentials),
+            });
 
-      toast.success(response!.message, {
-        style: {
-          borderRadius: '10px',
-          background: '#333',
-          color: '#fff',
-        },
-        duration: 6000,
-      });
+            const response = await res.json();
 
-      if (res.ok) {
-        router.push('/sign-in');
-      }
-    } catch (error: any) {
-      toast.error(error, {
-        style: {
-          borderRadius: '10px',
-          background: '#333',
-          color: '#fff',
-        },
-        duration: 6000,
-      });
-    } finally {
-      setLoading(false);
-    }
-  };
-  return (
-    <form action="" id={styles.container} onSubmit={handleSignup}>
-      <Toaster position="top-center" reverseOrder={true} />
-      <label htmlFor="name" className={styles.label}>
-        <span className={styles.text}>Please enter your name</span>
-        <input
-          type="text"
-          className={styles.input}
-          name="name"
-          id="name"
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-      <label htmlFor="email" className={styles.label}>
-        <span className={styles.text}>Please enter your email</span>
-        <input
-          type="text"
-          className={styles.input}
-          name="email"
-          id="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <label htmlFor="password" className={styles.label}>
-        <span className={styles.text}>Please create a Password</span>
-        <input
-          type={toggle ? 'text' : 'password'}
-          className={styles.input}
-          name="password"
-          id="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <label htmlFor="checkbox" id={styles.childLabel}>
-          <input
-            type="checkbox"
-            name="checkbox"
-            id="checkbox"
-            onChange={() => setToggle(!toggle)}
-          />
-          <span className={styles.text}>Make password visible.</span>
-        </label>
-      </label>
-      <button type="submit" id={styles.button} disabled={loading}>
-        {loading ? (
-          <ReactLoading type="spin" color="black" height={20} width={20} />
-        ) : (
-          'Sign up'
-        )}
-      </button>
-    </form>
-  );
+            // execute this snippet is to response is not okay
+            if (!res.ok) {
+                toast.error(response.message, {
+                    style: {
+                        borderRadius: '10px',
+                        background: '#333',
+                        color: '#fff',
+                    },
+                    duration: 6000,
+                });
+
+                return dispatch(failure(response.message));
+            }
+
+            // execute this snippet is to response is okay
+            if (res.ok) {
+                toast.success(response!.message, {
+                    style: {
+                        borderRadius: '10px',
+                        background: '#333',
+                        color: '#fff',
+                    },
+                    duration: 6000,
+                });
+
+                // push to user to the sign in page
+                router.push('/sign-in');
+
+                // initializing success reducer
+                return dispatch(success());
+            }
+        } catch (error: any) {
+            // sending error message
+            toast.error(error, {
+                style: {
+                    borderRadius: '10px',
+                    background: '#333',
+                    color: '#fff',
+                },
+                duration: 6000,
+            });
+
+            // initializing failure reducer
+            dispatch(failure(error));
+        }
+    };
+    return (
+        <form action="" id={styles.container} onSubmit={handleSignup}>
+            <Toaster position="top-center" reverseOrder={true} />
+            <label htmlFor="name" className={styles.label}>
+                <span className={styles.text}>Please enter your name</span>
+                <input
+                    type="text"
+                    className={styles.input}
+                    name="name"
+                    id="name"
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </label>
+            <label htmlFor="email" className={styles.label}>
+                <span className={styles.text}>Please enter your email</span>
+                <input
+                    type="text"
+                    className={styles.input}
+                    name="email"
+                    id="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </label>
+            <label htmlFor="password" className={styles.label}>
+                <span className={styles.text}>Please create a Password</span>
+                <input
+                    type={toggle ? 'text' : 'password'}
+                    className={styles.input}
+                    name="password"
+                    id="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <label htmlFor="checkbox" id={styles.childLabel}>
+                    <input
+                        type="checkbox"
+                        name="checkbox"
+                        id="checkbox"
+                        onChange={() => setToggle(!toggle)}
+                    />
+                    <span className={styles.text}>Make password visible.</span>
+                </label>
+            </label>
+            <button type="submit" id={styles.button} disabled={loading}>
+                {loading ? (
+                    <ReactLoading
+                        type="spin"
+                        color="black"
+                        height={20}
+                        width={20}
+                    />
+                ) : (
+                    'Sign up'
+                )}
+            </button>
+        </form>
+    );
 };
 
 export default SignupForm;
