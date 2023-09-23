1730edf1ca8b3ab8953109b27734240328d8e450
kunaalgaur <kunaalgaurr@gmail.com>
2023-09-23 07:57:32 +0530

    Verified JWT token

--- components/router/ProtectedRoute/ProtectedRoute.tsx
+++ components/router/ProtectedRoute/ProtectedRoute.tsx
@@ -1,27 +1,47 @@
 'use client';
 
-import { useAppSelector } from '@/redux/hooks';
+import toast, { Toaster } from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
+
+const token = localStorage.getItem('token') as string;
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const token =
-    useAppSelector((state) => state.signin.token) ||
-    localStorage.getItem('token');
-  const router = useRouter();
+    const router = useRouter();
 
-  useEffect(() => {
-    if (!token) {
-      return router.push('/sign-in');
-    }
-  }, [token]);
-  return (
-    <>
-      <Toaster position="top-center" reverseOrder={true} />
-      {children}
-    </>
-  );
+    const verifyToken = async () => {
+        try {
+            const res = await fetch('/api/auth/verify-token', {
+                method: 'POST',
+                body: JSON.stringify({ token }),
+            });
+
+            const response = await res.json();
+
+            if (!res.ok) {
+                toast.error(response.message);
+
+                return router.push('/sign-in');
+            }
+        } catch (error: any) {
+            return toast.error(error.message);
+        }
+    };
+
+    useEffect(() => {
+        verifyToken();
+
+        if (!token) {
+            return router.push('/sign-in');
+        }
+    }, [token]);
+
+    return (
+        <>
+            <Toaster position="top-center" reverseOrder={true} />
+            {children}
+        </>
+    );
 };
 
 export default ProtectedRoute;
