069c0c4632bc18de3d87c07a6884abb328d87eaf
kunaalgaur <kunaalgaurr@gmail.com>
2023-09-23 07:54:35 +0530

    Fetched Threads

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,32 @@
 import Thread from '@/components/card/Thread/Thread';
-import styles from './page.module.css';
-import { useAppSelector } from '@/redux/hooks';
+import { Post } from '@/types/type';
+import React from 'react';
 
-const getAllPosts = async () => {
-  const res = await fetch('')
-}
+let pageNumber = 1;
 
-export default function Home() {
-  return (
-    <div id={styles.container}>
-      <div></div>
-    </div>
-  );
-}
+const getThreads = async () => {
+    const res = await fetch(
+        `http://localhost:3000/api/thread/get-all-thread/?results=15&page=${pageNumber}`
+    );
+
+    if (!res.ok) {
+        throw new Error('An unknown error occured.');
+    }
+
+    return await res.json();
+};
+
+const page = async () => {
+    const threads: string[] = await getThreads();
+
+    console.log(threads);
+    return (
+        <div>
+            {threads.map((thread: Post) => {
+                return <Thread post={thread} />;
+            })}
+        </div>
+    );
+};
+
+export default page;
