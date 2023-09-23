9319b1b6882cbcb4156bdd21ef0e9ccbd6de8de7
kunaalgaur <kunaalgaurr@gmail.com>
2023-09-23 07:55:47 +0530

    Fixed bugs

--- app/api/thread/get-single-thread/[threadId]/route.ts
+++ app/api/thread/get-single-thread/[threadId]/route.ts
@@ -3,32 +3,34 @@ import connectDB from '@/lib/mongoose';
 import { NextResponse } from 'next/server';
 
 export const GET = async (
-  req: Request,
-  { params }: { params: { threadId: string } }
+    req: Request,
+    { params }: { params: { threadId: string } }
 ) => {
-  try {
-    await connectDB();
+    try {
+        await connectDB();
 
-    const threadId = params.threadId;
-    const thread = await Thread.findById(threadId);
-    if (!thread) {
-      return NextResponse.json({
-        status: 404,
-        name: 'Custom Error',
-        message: 'Thread not found.',
-      });
+        const threadId = params.threadId;
+
+        const thread = await Thread.findById(threadId);
+
+        if (!thread) {
+            return NextResponse.json({
+                status: 404,
+                name: 'Custom Error',
+                message: 'Thread not found.',
+            });
+        }
+
+        return NextResponse.json({
+            status: 200,
+            data: thread,
+            message: 'Thread found',
+        });
+    } catch (error: any) {
+        return NextResponse.json({
+            status: 500,
+            name: error.name,
+            message: error.message,
+        });
     }
-
-    return NextResponse.json({
-      status: 200,
-      data: thread,
-      message: 'Thread found',
-    });
-  } catch (error: any) {
-    return NextResponse.json({
-      status: 500,
-      name: error.name,
-      message: error.message,
-    });
-  }
 };
