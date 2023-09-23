2c3117d876e6d4acf5b5c32c3cb8029685ef0011
kunaalgaur <kunaalgaurr@gmail.com>
2023-09-23 07:58:00 +0530

    Update NavbarRight.tsx
    Added logout button functionality

--- components/shared/Navbar/NavbarRight/NavbarRight.tsx
+++ components/shared/Navbar/NavbarRight/NavbarRight.tsx
@@ -1,36 +1,57 @@
 'use client';
 
-import React, { useState } from 'react';
-import styles from './NavbarRight.module.css';
 import { HiMiniBars3BottomRight } from 'react-icons/hi2';
+import { logout } from '@/redux/slice/signinSlice';
+import { useAppDispatch } from '@/redux/hooks';
+import styles from './NavbarRight.module.css';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 const NavbarRight = () => {
-  const [toggle, setToggle] = useState(false);
+    const dispatch = useAppDispatch();
+    const router = useRouter();
 
-  const handleClick = () => {
-    return setToggle(!toggle);
-  };
+    const [toggle, setToggle] = useState(false);
 
-  return (
-    <div onClick={handleClick} id={styles.container}>
-      <HiMiniBars3BottomRight />
+    const handleClick = () => {
+        return setToggle(!toggle);
+    };
 
-      <div id={styles.wrapper} style={{ display: toggle ? 'flex' : 'none' }}>
-        <Link href="/about" className={styles.link}>
-          About
-        </Link>
+    const handleLogout = () => {
+        localStorage.clear();
 
-        <Link href="/settings" className={styles.link}>
-          Settings
-        </Link>
+        router.push('/sign-in');
 
-        <span className={styles.link} id={styles.logout}>
-          Log out
-        </span>
-      </div>
-    </div>
-  );
+        console.log('Hello World');
+
+        return dispatch(logout());
+    };
+
+    return (
+        <div onClick={handleClick} id={styles.container}>
+            <HiMiniBars3BottomRight />
+
+            <div
+                id={styles.wrapper}
+                style={{ display: toggle ? 'flex' : 'none' }}>
+                <Link href="/about" className={styles.link}>
+                    About
+                </Link>
+
+                <Link href="/settings" className={styles.link}>
+                    Settings
+                </Link>
+
+                <span
+                    className={styles.link}
+                    id={styles.logout}
+                    onClick={() => handleLogout}>
+                    Log out
+                </span>
+            </div>
+        </div>
+    );
 };
 
 export default NavbarRight;
