ee0778bd467670cdd0e2950604e3f83e3098a8b2
kunaalgaur <kunaalgaurr@gmail.com>
2023-09-24 14:25:35 +0530

    Added search page functionality.

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,7 +1,62 @@
-import React from 'react';
+'use client';
+
+import { HiOutlineMagnifyingGlass } from 'react-icons/hi2';
+import React, { useEffect, useState } from 'react';
+import styles from './page.module.css';
+import User from '@/components/card/User/User';
 
 const page = () => {
-  return <div>page</div>;
+    const [items, setItems] = useState<any[]>([]);
+    const [query, setQuery] = useState<string>('');
+
+    useEffect(() => {
+        const fetchItems = async () => {
+            try {
+                const res = await fetch('/api/user/get-all-user');
+
+                const response = await res.json();
+
+                if (!res.ok) {
+                    throw new Error('An unknown error happened.');
+                }
+
+                if (res.ok) {
+                    return setItems(response);
+                }
+            } catch (error: any) {
+                throw new Error(error.message);
+            }
+        };
+
+        fetchItems();
+    }, []);
+
+    const filteredItems = items.filter((item) => {
+        return item.name.toLowerCase().includes(query.toLowerCase());
+    });
+
+    return (
+        <div id={styles.container}>
+            <div id={styles.top}>
+                <div id={styles.wrapper}>
+                    <HiOutlineMagnifyingGlass id={styles.searchIcon} />
+                    <input
+                        type="text"
+                        name="search"
+                        value={query}
+                        onChange={(e) => setQuery(e.target.value)}
+                        id={styles.searchInput}
+                        placeholder="Search something..."
+                    />
+                </div>
+            </div>
+            <div>
+                {filteredItems.map((item: any) => {
+                    return <User user={item} />;
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default page;
---
+++ components/card/User/User.tsx
@@ -0,0 +1,25 @@
+import { User } from '@/types/type';
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './User.module.css';
+
+const User = ({ user }: { user: User }) => {
+    return (
+        <Link href={`/profile/${user._id}`} id={styles.container}>
+            <Image
+                src={user?.image || `/user.png`}
+                alt=""
+                height={50}
+                width={50}
+                id={styles.left}
+            />
+
+            <div id={styles.right}>
+                <span id={styles.name}>{user?.name}</span>
+                <span id={styles.username}>{user?.username}</span>
+            </div>
+        </Link>
+    );
+};
+
+export default User;
