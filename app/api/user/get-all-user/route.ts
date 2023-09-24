0882164b183955a3f5b9b82eb87f0908f5310a5d
kunaalgaur <kunaalgaurr@gmail.com>
2023-09-24 14:27:16 +0530

    Removed query params

--- app/api/user/get-all-user/route.ts
+++ app/api/user/get-all-user/route.ts
@@ -3,43 +3,21 @@ import connectDB from '@/lib/mongoose';
 import { NextResponse } from 'next/server';
 
 export const GET = async (req: Request) => {
-  try {
-    await connectDB();
+    try {
+        await connectDB();
 
-    const queryParam = new URL(req.url).searchParams.get('query');
+        const users = await User.find();
 
-    if (!queryParam) {
-      return NextResponse.json({
-        status: 400,
-        message: 'Missing "query" parameter',
-      });
+        return NextResponse.json(users);
+    } catch (error: any) {
+        return NextResponse.json(
+            {
+                name: error.name,
+                message: error.message,
+            },
+            {
+                status: 500,
+            }
+        );
     }
-
-    const users = await User.find({
-      $or: [
-        { name: { $in: [queryParam] } },
-        { username: { $in: [queryParam] } },
-      ],
-    });
-
-    if (!users || users.length === 0) {
-      return NextResponse.json({
-        status: 404,
-        name: 'Custom Error',
-        message: 'No users found by the provided criteria',
-      });
-    }
-
-    return NextResponse.json({
-      status: 200,
-      data: users,
-      message: 'Users found.',
-    });
-  } catch (error: any) {
-    return NextResponse.json({
-      status: 500,
-      name: error.name,
-      message: error.message,
-    });
-  }
 };
