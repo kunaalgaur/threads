247f0d362b0fa138f12a2a55bf4937da4f05cb18
kunaalgaur <kunaalgaurr@gmail.com>
2023-09-24 14:23:42 +0530

    Created update profile feature

---
+++ app/(root)/edit-profile/[userId]/page.tsx
@@ -0,0 +1,13 @@
+import EditUserForm from '@/components/form/EditUserForm/EditUserForm';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <span>Edit your profile</span>
+            <EditUserForm />
+        </div>
+    );
+};
+
+export default page;
---
+++ app/api/user/edit-user/[userId]/route.ts
@@ -0,0 +1,55 @@
+import User from '@/lib/models/User.model';
+import connectDB from '@/lib/mongoose';
+import { NextResponse } from 'next/server';
+
+export const PUT = async (req: Request, params: { userId: string }) => {
+    try {
+        connectDB();
+
+        const { currentUserId, name, image, bio } = await req.json();
+        const userId = params.userId;
+
+        if (!userId !== currentUserId) {
+            return NextResponse.json(
+                {
+                    message: 'You cannot access this resource.',
+                },
+                {
+                    status: 403,
+                }
+            );
+        }
+
+        const user = await User.findById(userId);
+        if (!user) {
+            return NextResponse.json(
+                {
+                    message: 'User not found',
+                },
+                {
+                    status: 403,
+                }
+            );
+        }
+
+        await user.updateOne({
+            name: name,
+            image: image,
+            bio: bio,
+        });
+
+        return NextResponse.json({
+            messgage: 'User updated successfully.',
+        });
+    } catch (error: any) {
+        return NextResponse.json(
+            {
+                name: error.name,
+                message: error.message,
+            },
+            {
+                status: 500,
+            }
+        );
+    }
+};
