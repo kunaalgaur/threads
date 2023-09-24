d87ecb19a80d5d178fa68540aca554d6099221c8
kunaalgaur <kunaalgaurr@gmail.com>
2023-09-24 14:25:58 +0530

    Minor changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,26 +5,41 @@ import React from 'react';
 let pageNumber = 1;
 
 const getThreads = async () => {
-    const res = await fetch(
-        `http://localhost:3000/api/thread/get-all-thread/?results=15&page=${pageNumber}`
-    );
+    try {
+        const res = await fetch(
+            `http://localhost:3000/api/thread/get-all-thread/?results=15&page=${pageNumber}`
+        );
 
-    if (!res.ok) {
-        throw new Error('An unknown error occured.');
+        if (!res.ok) {
+            throw new Error('An unknown error occurred.');
+        }
+
+        if (res.ok) {
+            const data = await res.json();
+
+            // Assuming the response contains an array of Post objects
+            const threads: Post[] = data;
+
+            return threads;
+        }
+    } catch (error: any) {
+        throw new Error(error.message);
     }
-
-    return await res.json();
 };
 
 const page = async () => {
-    const threads: string[] = await getThreads();
+    const threads: Post[] | undefined = await getThreads();
 
     console.log(threads);
     return (
         <div>
-            {threads.map((thread: Post) => {
-                return <Thread post={thread} />;
-            })}
+            {threads?.length === 0 ? (
+                <span>There are no posts to show.</span>
+            ) : (
+                threads?.map((thread: Post) => {
+                    return <Thread post={thread} key={thread._id} />;
+                })
+            )}
         </div>
     );
 };
