978cf797dc2f45ce118370880ed621f2855fb4fd
kunaalgaur <kunaalgaurr@gmail.com>
2023-09-25 19:22:59 +0530

    Fixed onboarding errors

---
+++ app/api/auth/onboarding/[userId]/route.ts
@@ -0,0 +1,44 @@
+import User from '@/lib/models/User.model';
+import connectDB from '@/lib/mongoose';
+import { NextResponse } from 'next/server';
+
+export const PUT = async (
+    req: Request,
+    { params }: { params: { userId: string } }
+) => {
+    try {
+        await connectDB();
+
+        const userId = params.userId;
+        const { image, username, bio } = await req.json();
+
+        const user = await User.findOne({ username: username });
+        if (user) {
+            return NextResponse.json({
+                status: 404,
+                name: 'Custom Error',
+                message:
+                    'This user name is already taken, please try another username.',
+            });
+        }
+
+        const userLookUp = await User.findById(userId);
+
+        await userLookUp.updateOne({
+            image: image,
+            username: username,
+            bio: bio,
+        });
+
+        return NextResponse.json({
+            status: 200,
+            message: 'You are now onboarded.',
+        });
+    } catch (error: any) {
+        return NextResponse.json({
+            status: 500,
+            name: error.name,
+            message: error.message,
+        });
+    }
+};
