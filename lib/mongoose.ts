85696a374d9acc0e78b6acd246368462fd2f791b
kunaalgaur <kunaalgaurr@gmail.com>
2023-09-25 09:57:30 +0530

    Updated mongoose.ts configuration

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -4,26 +4,32 @@ import mongoose from 'mongoose';
 let isConnected = false;
 
 const connectDB = async () => {
-  // Check if Mongo URI is valid or not
-  if (!process.env.MONGO_URI) {
-    return console.log(
-      chalk.red('Error: Mongo URI is either invalid or missing.')
-    );
-  }
+    // Check if Mongo URI is valid or not
+    if (!process.env.MONGO_URI) {
+        return console.log(
+            chalk.red('Error: Mongo URI is either invalid or missing.')
+        );
+    }
 
-  //   Check if MongoDB is already connected or not
-  if (isConnected) {
-    return console.log(chalk.green('Already connected to Mongo DB.'));
-  }
+    //   Check if MongoDB is already connected or not
+    if (isConnected) {
+        return console.log(chalk.green('Already connected to Mongo DB.'));
+    }
 
-  try {
-    // Securing a connection with the MongoDB
-    await mongoose.connect(process.env.MONGO_URI);
-    isConnected = true;
-    return console.log(chalk.green('MongoDB is connected securely.'));
-  } catch (error: any) {
-    throw new Error('There was an unexpected error in connecting to MongoDB.');
-  }
+
+    try {
+        console.log({ isConnected });
+        // Securing a connection with the MongoDB
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log({ isConnected });
+
+        isConnected = true;
+        return console.log(chalk.green('MongoDB is connected securely.'));
+    } catch (error: any) {
+        throw new Error(
+            'There was an unexpected error in connecting to MongoDB.'
+        );
+    }
 };
 
 export default connectDB;
