ea23abb7d0e882ccc787b442d82585e9be55c12b
kunaalgaur <kunaalgaurr@gmail.com>
2023-09-26 00:31:03 +0530

    Created new reducers

--- redux/store.ts
+++ redux/store.ts
@@ -4,6 +4,7 @@ import threadReducer from './slice/threadSlice';
 import signupReducer from './slice/signupSlice';
 import onboardingSlice from './slice/onboardingSlice';
 import profileSlice from './slice/profileSlice';
+import getThreadsSlice from './slice/getThreadsSlice';
 
 export const store = configureStore({
     reducer: {
@@ -11,7 +12,8 @@ export const store = configureStore({
         signin: signinReducer,
         thread: threadReducer,
         onboarding: onboardingSlice,
-        profile: profileSlice
+        profile: profileSlice,
+        getThreads: getThreadsSlice,
     },
 });
 
