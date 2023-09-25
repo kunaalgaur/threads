054ada75771005bffcf91786c3d426253aa84635
kunaalgaur <kunaalgaurr@gmail.com>
2023-09-26 00:33:34 +0530

    (Pending)

--- app/(root)/settings/[userId]/page.tsx
+++ app/(root)/settings/[userId]/page.tsx
@@ -1,7 +1,32 @@
+import { currentUserId } from '@/constants/variable';
+import Link from 'next/link';
 import React from 'react';
 
 const page = () => {
-    return <div>page</div>;
+    return (
+        <div>
+            <span>Settings</span>
+            <div>
+                <span>Change you username</span>
+                <span></span>
+            </div>
+            <div>
+                <span>Change you email address</span>
+            </div>
+            <div>
+                <span>Change password</span>
+            </div>
+            <div>
+                <span>Switch to public account?</span>
+            </div>
+            <div>
+                <span>Delete Account?</span>
+            </div>
+            <div>
+                <span>Log out</span>
+            </div>
+        </div>
+    );
 };
 
 export default page;
