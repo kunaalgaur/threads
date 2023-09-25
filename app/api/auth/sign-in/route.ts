9f538d4afe441ed6d6e700e701c1f57a4f9680a0
kunaalgaur <kunaalgaurr@gmail.com>
2023-09-25 19:23:43 +0530

    Minor changes

--- app/api/auth/sign-in/route.ts
+++ app/api/auth/sign-in/route.ts
@@ -3,56 +3,59 @@ import connectDB from '@/lib/mongoose';
 import { NextResponse } from 'next/server';
 
 export const POST = async (req: Request) => {
-  try {
-    // Create a connection with DB
-    await connectDB();
+    try {
+        // Create a connection with DB
+        await connectDB();
 
-    const { email, password } = await req.json();
+        const { email, password } = await req.json();
 
-    const user = await User.findOne({ email: email });
-    if (!user) {
-      return NextResponse.json({
-        status: 404,
-        name: 'Custom Error',
-        message: 'This email is not registered, try signing up.',
-      });
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            return NextResponse.json(
+                {
+                    message: 'This email is not registered, try signing up.',
+                },
+                {
+                    status: 401,
+                }
+            );
+        }
+
+        const validatePassword = await user.comparePassword(password);
+        if (!validatePassword) {
+            return NextResponse.json(
+                {
+                    message:
+                        'Incorrect password, please enter correct password to continue.',
+                },
+                { status: 401 }
+            );
+        }
+
+        const token = await user.getJWTToken();
+
+        const userImpData = {
+            ...user.toObject(),
+            password: undefined,
+            followers: undefined,
+            followings: undefined,
+            isPrivate: undefined,
+            forgotPasswordToken: undefined,
+            forgotPasswordTokenExpiry: undefined,
+            verifyToken: undefined,
+            verifyTokenExpiry: undefined,
+        };
+
+        return NextResponse.json({
+            user: userImpData,
+            token: token,
+            message: 'You are successfully logged in.',
+        });
+    } catch (error: any) {
+        return NextResponse.json({
+            status: 500,
+            name: error.name,
+            message: error.message,
+        });
     }
-
-    const validatePassword = await user.comparePassword(password);
-    if (!validatePassword) {
-      return NextResponse.json(
-        {
-          message:
-            'Incorrect password, please enter correct password to continue.',
-        },
-        { status: 403 }
-      );
-    }
-
-    const token = await user.getJWTToken();
-
-    const userImpData = {
-      ...user.toObject(),
-      password: undefined,
-      followers: undefined,
-      followings: undefined,
-      isPrivate: undefined,
-      forgotPasswordToken: undefined,
-      forgotPasswordTokenExpiry: undefined,
-      verifyToken: undefined,
-      verifyTokenExpiry: undefined,
-    };
-
-    return NextResponse.json({
-      user: userImpData,
-      token: token,
-      message: 'You are successfully logged in.',
-    });
-  } catch (error: any) {
-    return NextResponse.json({
-      status: 500,
-      name: error.name,
-      message: error.message,
-    });
-  }
 };
